64d63186f327056e6a9124192833f37927be3c19
CDU_Ge <cosplox@outlook.com>
2024-08-18 21:22:20 +0800

    update version to 0.0.2 and update api

--- qy/__init__.py
+++ qy/__init__.py
@@ -1,12 +1,24 @@
 # coding: utf-8
 
-__all__ = ['qy', 'symbol', 'symbolproxy']
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Ge'
+__all__ = [
+    'qy',
+    'symbol',
+    'symbolproxy',
+    't', 'T',
+    'nil', 'NIL',
+    'quote', 'atom', 'eq', 'car', 'cdr', 'cons', 'cond'
+]
 
 """
 Created on 2024-08-15
 
 """
 
-from qy.core import qy, symbol, symbolproxy
+from qy.core import symbol, symbolproxy
+from qy.operator import qy, T, NIL
+from qy.operator import quote, atom, eq, car, cdr, cons, cond
+
+t = T
+nil = NIL
