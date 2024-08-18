8034bdbd3e6aac6da4bbc365f3a58760cc64c924
CDU_Ge <cosplox@outlook.com>
2024-08-18 22:41:43 +0800

    add REPL base on python

---
+++ qy/__main__.py
@@ -0,0 +1,4 @@
+import qy.cli
+
+if __name__ == '__main__':
+    qy.cli.main()
--- qy/cli.py
+++ qy/cli.py
@@ -4,8 +4,15 @@
 Created on 2024-08-15
 
 """
+import code
+from qy import *
+
+LOCALS = globals()
 
 
 def main():
-    print('Qy Not Support Cli Now!')
-    print('Please wait for the next version!')
+    code.interact(
+        banner='Qy Lang, a Lisp language implemented in and based on Python',
+        readfunc=lambda prompt='': f"qy.eval((print, {input(prompt)}))",
+        local=LOCALS,
+        exitmsg='Goodbye!')
