13f3a2b9ced3337b8d7792a7e708a6ebbc785bdc
CDU_Ge <cosplox@outlook.com>
2024-08-27 13:30:50 +0800

    fix bug

--- qy/operators/__init__.py
+++ qy/operators/__init__.py
@@ -4,7 +4,8 @@ import os
 
 def qimap(func, *iterables):
     with concurrent.futures.ThreadPoolExecutor(
-        max_workers=os.getenv("QY_MAX_WORKERS", None)
+        max_workers=int(os.getenv("QY_MAX_WORKERS",
+                        min(32, os.cpu_count() + 4)))
     ) as executor:
         yield from executor.map(func, *iterables)
 
