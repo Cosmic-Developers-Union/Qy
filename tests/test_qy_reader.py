2c5067dfe670b5980e84e815936a5a2ec7b9ece6
CDU_Ge <cosplox@outlook.com>
2024-08-29 10:49:58 +0800

    add test_qy_reader

---
+++ tests/test_qy_reader.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+import sys
+import unittest
+import lark
+GRAMMER = f"""
+?start: expressions
+expressions: expression*
+
+?expression: atom
+    | list
+    | quote_expression -> quote
+
+?quote_expression: "'" expression
+?list: "(" expression* ")"
+?atom: STRING   -> string
+    | SYMBOL    -> symbol
+STRING: /"[^"]*"/
+SYMBOL: /[a-zA-Z0-9_\-@:\.]+/
+%import common.WS
+%ignore WS
+"""
+
+
+class TestReader(unittest.TestCase):
+    def setUp(self):
+        sys.path.insert(0, '')
+
+    def test_(self):
+        from qy.reader import tokenize
+        from qy import symbol
+        tokens = tokenize(Path('examples/data.qy').read_text('utf-8'))
+        print(list(tokens))
+        parser = lark.Lark(GRAMMER)
+        tree = parser.parse(Path('examples/data.qy').read_text('utf-8'))
+        print(tree.pretty())
+
+        @lark.v_args(inline=True)
+        class QyTransformer(lark.Transformer):
+            def expressions(self, *tokens):
+                return list(tokens)
+
+            def quote(self, tokens):
+                return ('quote', tokens)
+
+            def string(self, token):
+                return str(token[1:-1])
+
+            def symbol(self, name):
+                return symbol(name)
+
+            def list(self, *items):
+                return items
+        transformer = QyTransformer()
+        expr = transformer.transform(tree)
+        print(expr)
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
