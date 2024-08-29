ea087c5fb58a531a9f6c2e1f94d48b9b432108b9
CDU_Ge <cosplox@outlook.com>
2024-08-29 13:34:01 +0800

    add deps

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-from qy import __version__
+import re
+from pathlib import Path
+
+
+def get_version():
+    code = Path('qy/__init__.py').read_text(encoding='utf-8')
+    return re.search(r"^__version__ = '([^']+)'", code, re.M).group(1)
+
+
 setup(
     name='QyLang',
-    version=__version__,
+    version=get_version(),
     description='Qy Lang, a Lisp language implemented in and based on Python',
     long_description=Path('README.md').read_text(encoding='utf-8'),
     long_description_content_type='text/markdown',
     author='Ge',
     author_email='cosplox@outlook.com',
     url='https://github.com/Cosmic-Developers-Union/Qy',
+    install_requires=[
+        'lark>=1.2.2'
+    ],
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -18,9 +29,6 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.8',
-    install_requires=[
-        # Add any dependencies your package requires here
-    ],
     entry_points={
         'console_scripts': [
             'qy=qy.cli:main',  # Assuming you have a CLI entry point
